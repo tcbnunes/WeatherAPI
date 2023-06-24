41dad4dea3c58bc881b536ec1eb2237da4970b87
tcbnunes <106353741+tcbnunes@users.noreply.github.com>
2023-06-24 14:42:16 -0300

    Last card fix

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log (temperature)
 
 const apiKey = "2a4eccb32b5e17e647d64325775f855b"
 
-var cityName = "Denver"
+var cityName = "Goiania"
 
 var apiCall = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
 
@@ -33,31 +33,32 @@ async function logJSONData() {
 async function showData () {
     const data = await logJSONData()
     const dateStr = data.list[0].dt_txt.slice(0,10).replaceAll("-","/")
-   cityNameElement.innerText = `${data.city.name}(${dateStr})`
+    cityNameElement.innerText = `${data.city.name}(${dateStr})`
 
-   for (let index = 0; index < 6; index ++) {
-    if (index == 5) {
-        temperature[index].innerText = `Temp: ${data.list[39].main.temp} F`
+    for (let index = 0; index < 6; index ++) {
+        let listIndex = index *8;
+        if (index == 5) {
+            temperature[index].innerText = `Temp: ${data.list[39].main.temp} F`
 
-   windSpeed[index].innerText = `Wind: ${data.list[39].wind.speed} MPH`
-
-
-   humidity[index].innerText = `Humidity: ${data.list[39].main.humidity}%`
-
-//    icons[index].setAttribute( "src",`https://openweathermap.org/img/wn/${data.list[39].weather.icon}@2x.png`)
-    }
-
-    temperature[index].innerText = `Temp: ${data.list[index * 8].main.temp} F`
-
-   windSpeed[index].innerText = `Wind: ${data.list[index * 8].wind.speed} MPH`
+            windSpeed[index].innerText = `Wind: ${data.list[39].wind.speed} MPH`
 
 
-   humidity[index].innerText = `Humidity: ${data.list[index * 8].main.humidity}%`
+            humidity[index].innerText = `Humidity: ${data.list[39].main.humidity}%`
+            date[4].innerText = data.list[39].dt_txt.slice(0,10).replaceAll("-","/")
 
-//    icons[index].setAttribute( "src",`https://openweathermap.org/img/wn/${data.list[index * 8].weather.icon}@2x.png`)
-    if (index !== 0) {
-        date[index - 1].innerText = data.list[index * 8].dt_txt.slice(0,10).replaceAll("-","/")
-    }
+        //  icons[index].setAttribute( "src",`https://openweathermap.org/img/wn/${data.list[39].weather.icon}@2x.png`)
+        }
+
+        temperature[index].innerText = `Temp: ${data.list[index * 8].main.temp} F`
+
+        windSpeed[index].innerText = `Wind: ${data.list[index * 8].wind.speed} MPH`
+
+        humidity[index].innerText = `Humidity: ${data.list[index * 8].main.humidity}%`
+
+        //    icons[index].setAttribute( "src",`https://openweathermap.org/img/wn/${data.list[index * 8].weather.icon}@2x.png`)
+        if (index !== 0) {
+            date[index - 1].innerText = data.list[index * 8].dt_txt.slice(0,10).replaceAll("-","/")
+        }
 }
 }
 
